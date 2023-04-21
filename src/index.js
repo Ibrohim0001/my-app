d8af2edf4bb45fec5b4e0968dac87d5a47fdfe7c
Ibrohim0001 <ibrohimmamadjanof@gmail.com>
2023-04-22 02:05:14 +0900

    started

--- src/index.js
+++ src/index.js
@@ -1,15 +1,47 @@
-
-
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css"
 
-function Greeting() {
-  return ( 
-    <div>
-      <h2>Hello world</h2>
-    </div>
-  )
 
-}
+ 
+const BookList = () => {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+};
+
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => (
+  <img
+    src="https://cwexplorationphotography.com/wp-content/uploads/galleries/post-1540/thumbnails/10896225_528254790650735_6470305242078852429_o.jpg"
+    alt="image "
+  />
+);
+
+const Title = () => <h2>Book Title</h2>;
+
+const Author = () => {
+  return <h4>image placeholder</h4>;
+};
+
+
+
+
+
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Greeting />);
+root.render(<BookList />);

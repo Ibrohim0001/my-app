ffa717b2fb84a25d3a768a9a743143b3f1e0b5db
Ibrohim0001 <ibrohimmamadjanof@gmail.com>
2023-04-23 16:42:39 +0900

    keep going

--- src/index.js
+++ src/index.js
@@ -1,31 +1,47 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
- 
+
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        author="Patrick Rothuss"
+        names="The Name of Wind"
+        photo="./Images/image1.jpg"
+      >
+        <p>THis boo kis one of the most expensive books in the wolrd</p>
+        <button>Buy now</button>
+        </Book>
+      <Book
+        author="WALTER Thirring"
+        names="Code Name God"
+        photo="./Images/image2.jpg"
+      />
+      <Book
+        author="Shannon W.Lueld"
+        names="Another Side Of The Moon"
+        photo="./Images/image3.jpg"
+      />
     </section>
   );
 };
 
-const Book = () => {
-  const title = 'Wellcome to book shop'
+const photo = "./Images/image1.jpg";
+const names = "Wellcome to book shop";
+const author = "how are man";
+const title = "it is photo";
+
+const Book = ({ title, names, photo, author, children }) => {
   return (
     <article className="book">
-    <img
-     src="./Images/image1.jpg" alt="image " 
-     />
-     <h2>{title}</h2>
-     <h4>image placeholder</h4>
+      <img src={photo} alt={title} />
+      <h2>{names}</h2>
+      <h4>{author}</h4>
+      {children}
     </article>
   );
 };
-
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<BookList />);
